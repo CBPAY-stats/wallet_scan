18b6f14fcfe6acc95c183b0d557200b269c8d666
XDB Explorer <xdb@explorer.com>
2025-07-08 05:56:00 -0400

    Fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/wallet_scan/', // Set the base path for GitHub Pages
 })
 
